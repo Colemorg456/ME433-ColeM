a814e5ee8e909aa5385afd68ff7d546902ed035d
Colemorg456 <colemorg456@gmail.com>
2025-05-08 00:06:52 -0500

    HW7 OLED basic function
    Basic OLED functioning now

--- HW7/OLED_I2C/OLED_I2C.c
+++ HW7/OLED_I2C/OLED_I2C.c
@@ -2,6 +2,7 @@
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
 #include "ssd1306.h"
+#include "font.h"
 
 // I2C defines
 // This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
@@ -11,7 +12,8 @@
 #define I2C_SCL 17
 #define LED_PIN 25
 
-
+void drawMessage(int x, int y, char * m);
+void drawLetter(int x, int y, char c);
 
 int main()
 {
@@ -24,23 +26,58 @@ int main()
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
     gpio_pull_up(I2C_SDA);
     gpio_pull_up(I2C_SCL);
-    // For more examples of I2C use see https://github.com/raspberrypi/pico-examples/tree/master/i2c
 
     //Heartbeat LED
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
     bool LED_state = false;
 
+    //OLED startup
     ssd1306_setup();
     ssd1306_clear();
     ssd1306_update();
 
     while (true) {
-        char message[50];
-
         //toggle the heartbeat
         LED_state = !LED_state;
         gpio_put(LED_PIN, LED_state);
+
+        //Clear OLED
+        ssd1306_clear();
+
+        // Sprintf to show on screen
+        int i = 15;
+        char message[50];
+        sprintf(message, "My var = %d", i);
+        drawMessage(20,10,message); // draw starting at x=20,y=10  
+
+        //Push to OLED
+        ssd1306_update();
+
         sleep_ms(1000);
+    }
+}
+
+//Function for drawing the message and moving over a space
+void drawMessage(int x, int y, char * m){
+    int i = 0;
+    while(m[i]!=0){
+        drawLetter(x+i*6,y,m[i]);
+        i++;
+    }
+}
+
+//Function for drawinf the 5x8 bit letters
+void drawLetter(int x, int y, char c){
+    int row, col;
+    row = c - 0x20;
+    for(col=0; col<5; col++){
+        char byte = ASCII[row][col];
+
+        for(int i=0;i<8;i++){
+            char on_or_off = (byte>>i)&0b1;
+
+            ssd1306_drawPixel(x+col,y+i,on_or_off);
+        }
     }
 }
