a2177f40351f620115fce714547fb9ffa06618a9
Colemorg456 <colemorg456@gmail.com>
2025-05-11 20:32:31 -0500

    HW10 Part6

---
+++ HW10/part6.py
@@ -0,0 +1,85 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+
+avg_data = []
+
+t = [] # column 0
+data1 = [] # column 1
+data2 = [] # column 2
+
+#Manually change which CSV youre looking at every time
+with open("sigD.csv") as f:
+    # open the csv file
+    reader = csv.reader(f)
+    for row in reader:
+        # read the rows 1 one by one
+        t.append(float(row[0])) # leftmost column
+        data1.append(float(row[1])) # second column
+        # data2.append(float(row[2])) # third column
+
+#Moving average filter
+# X = 1000
+# for i in range(len(data1)):
+#     avg = 0
+#     if i<X:
+#         avg_data.append(data1[i])
+#     else:
+#         for j in range(X):
+#             avg = avg + data1[i-j]
+#         avg = avg/X
+#         avg_data.append(avg)
+
+#IIR/infinite impulse response Low-pass Filter 
+A =0.995
+B = 0.005
+for i in range(len(data1)):
+    if i==0: #first loop through
+        avg_data.append(data1[i])
+    else:
+        IIR = A*avg_data[i - 1] + B*data1[i]
+        avg_data.append(IIR)
+
+
+def fft_plotting(t, data1, avg_data):
+    
+    t =np.array(t)
+    y =np.array(data1)
+    y2 = np.array(avg_data)
+
+    Fs = (len(t))/(t[-1]) # sample rate
+    n = len(y) # length of the signal
+    k = np.arange(n)
+    T = n/Fs
+    frq = k/T # two sides frequency range
+    frq = frq[range(int(n/2))] # one side frequency range
+    Y = np.fft.fft(y)/n # fft computing and normalization
+    Y = Y[range(int(n/2))]
+
+    FY = np.fft.fft(y2)/n # fft computing and normalization
+    FY = FY[range(int(n/2))]
+
+    fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, figsize=(10,10))
+    fig.suptitle(f'Unfiltered vs. Filtered Data A={A} B={B}')
+
+    ax1.plot(t,y,color='black',label='Original Data')
+    ax1.set_xlabel('Time')
+    ax1.set_ylabel('Amplitude')
+    ax1.legend()
+
+    ax2.plot(t,avg_data,color='red',label='Filtered Data')
+    ax2.set_xlabel('Time')
+    ax2.set_ylabel('Amplitude')
+    ax2.legend()
+
+    ax3.loglog(frq,abs(Y),color='black',label='Original Data') # plotting the fft
+    ax3.set_xlabel('Freq (Hz)')
+    ax3.set_ylabel('|Y(freq)|')
+
+    ax4.loglog(frq,abs(FY),color='red',label='Filtered Data') # plotting the fft
+    ax4.set_xlabel('Freq (Hz)')
+    ax4.set_ylabel('|Y(freq)|')
+    plt.tight_layout()
+    plt.show()
+
+fft_plotting(t, data1, avg_data)
