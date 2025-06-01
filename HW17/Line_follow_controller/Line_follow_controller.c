3374d3a7db48444e66ce978460fbf21ddfbe40d8
Colemorg456 <colemorg456@gmail.com>
2025-06-01 15:34:08 -0500

    HW 17 motor controller basics
    Basics of motor controller except I dont have the input for the camera. Using the OLED screen to see the duty cycle instead of two LEDs

---
+++ HW17/Line_follow_controller/Line_follow_controller.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "ssd1306.h"
+#include "font.h"
+#include "hardware/pwm.h"
+
+
+//Motor Defines
+#define B_PWM 21
+#define B_PHASE 20
+#define A_PWM 19
+#define A_PHASE 18
+
+//OLED Screen defines
+#define I2C_PORT2 i2c0
+#define I2C_SDA2 16
+#define I2C_SCL2 17
+#define LED_PIN 25
+
+//Motor Controller defines
+#define MAX_DUTY 100  //Max duty cycle the robot will use when going around the track
+#define DEAD_BAND 5      //Dead band error space for line tracking
+#define STEERING_GAIN 1.0 //P proportional gain for controller
+
+//updating the OLED screen instead of using 2 LEDs
+void drawMessage(int x, int y, char * m);
+void drawLetter(int x, int y, char c);
+
+int main()
+{
+    stdio_init_all();
+
+    // I2C Initialisation
+    i2c_init(I2C_PORT2, 1000*1000);
+    gpio_set_function(I2C_SDA2, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL2, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA2);
+    gpio_pull_up(I2C_SCL2);
+
+    //Heartbeat LED
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+    bool LED_state = false;
+
+    //OLED startup
+    ssd1306_setup();
+    ssd1306_clear();
+    ssd1306_update();
+
+    //Hbridge directional pins setup
+    gpio_init(B_PHASE);
+    gpio_set_dir(B_PHASE, GPIO_OUT);
+    gpio_put(B_PHASE,1);
+    gpio_init(A_PHASE);
+    gpio_set_dir(A_PHASE, GPIO_OUT);
+    gpio_put(A_PHASE,1);
+
+    //PWM Initialization
+    float div = 1; // must be between 1-255
+    uint16_t wrap = 65535; // when to rollover, must be less than 65535
+    gpio_set_function(B_PWM, GPIO_FUNC_PWM);
+    uint slice_B = pwm_gpio_to_slice_num(B_PWM); // Get PWM slice number
+    pwm_set_clkdiv(slice_B, div); // divider
+    pwm_set_wrap(slice_B, wrap);
+    pwm_set_enabled(slice_B, true); // turn on the PWM
+    gpio_set_function(A_PWM, GPIO_FUNC_PWM);
+    uint slice_A = pwm_gpio_to_slice_num(A_PWM); // Get PWM slice number
+    pwm_set_clkdiv(slice_A, div); // divider
+    pwm_set_wrap(slice_A, wrap);
+    pwm_set_enabled(slice_A, true); // turn on the PWM
+
+    while (true) {
+        //Heartbeat LED
+        LED_state = !LED_state;
+        gpio_put(LED_PIN, LED_state);
+
+        //REPLACE LATER with input from teh camera so I can update in real time
+        int line_position = 15; 
+
+        //Line following controller
+        static int left_duty,right_duty;
+        if (abs(line_position) < DEAD_BAND){ //Robot is directly on the line
+            //Go straight, max PWM
+            left_duty = MAX_DUTY;
+            right_duty = MAX_DUTY;
+        }else if (line_position > 0){ //The line is going to the right
+            //Slow the right wheel while keeping the left wheel going
+            left_duty = MAX_DUTY;
+            right_duty = MAX_DUTY - (int)(STEERING_GAIN * abs(line_position));
+        }else {
+            //Line is going to left, slow left wheel and speed up right wheel
+            left_duty = MAX_DUTY - (int)(STEERING_GAIN*abs(line_position));
+            right_duty = MAX_DUTY;
+        }
+
+        // Clamp duty cycles to valid range
+        if(left_duty < 0){
+            left_duty = 0;
+        }
+        if(right_duty < 0){
+            right_duty = 0;
+        }
+
+        //Motor Control
+        uint16_t PWM_speed_right = (uint16_t)(wrap*(right_duty/100.0));
+        uint16_t PWM_speed_left = (uint16_t)(wrap*(left_duty/100.0));
+        gpio_put(B_PHASE,1);
+        gpio_put(A_PHASE,0);
+        pwm_set_gpio_level(A_PWM,PWM_speed_right);
+        pwm_set_gpio_level(B_PWM,PWM_speed_left);
+
+        //OLED Display updating Duty cycles
+        ssd1306_clear();
+        char left_msg[50];
+        char right_msg[50];
+        sprintf(left_msg,"Left duty cycle: %d",left_duty);
+        sprintf(right_msg,"Right duty cycle: %d",right_duty);
+        drawMessage(0,5,left_msg);
+        drawMessage(0,15,right_msg);
+        ssd1306_update();
+        sleep_ms(10);
+    }
+}
